18762bd81eb20116fdb9d6cb861fac68912874ff
Graham Christensen <graham@grahamc.com>
2017-10-10 07:17:57 -0400

    Ruin the nixos-config path, and keep the channels up to date on the target

--- modules/standard/default.nix
+++ modules/standard/default.nix
@@ -39,6 +39,25 @@ in {
 
     nix = {
       useSandbox = true;
+      nixPath = [
+        # Ruin the config so we don't accidentally run
+        # nixos-rebuild switch on the host
+        (let
+          cfg = pkgs.writeText "configuration.nix"
+            ''
+              assert builtins.trace "Hey dummy, you're on your server! Use NixOps!" false;
+              {}
+            '';
+         in "nixos-config=${cfg}")
+
+         # Copy the channel version from the deploy host to the target
+         "nixpkgs=/run/current-system/nixpkgs"
+      ];
     };
+
+    system.extraSystemBuilderCmds = ''
+      ln -sv ${pkgs.path} $out/nixpkgs
+    '';
+    environment.etc.host-nix-channel.source = pkgs.path;
   };
 }
