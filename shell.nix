d10b3badbd59a05fbf41d5851694f50f6e3d5867
Graham Christensen <graham@grahamc.com>
2017-10-10 07:33:09 -0400

    Better HISTFILE spec thanks to tilpner

--- shell.nix
+++ shell.nix
@@ -20,6 +20,6 @@ in stdenv.mkDerivation rec {
     export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
     export NIXOS_EXTRA_MODULE_PATH=$mod_root/modules/default.nix
     export NIXOPS_DEPLOYMENT="personal"
-    export HISTFILE=$(pwd)/.bash_hist
+    export HISTFILE="${builtins.toString ./.}/.bash_hist"
   '';
 }
