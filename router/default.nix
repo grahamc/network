2e4c77b7ed7543fc418f1a3aefb13b8bede284e1
Graham Christensen <graham@grahamc.com>
2017-12-24 16:40:07 -0500

    Setup the pxe support for console access

--- router/default.nix
+++ router/default.nix
@@ -218,8 +218,28 @@ in
   };
 
   services.tftpd = let
-    netboot = (import <nixpkgs/nixos/release.nix> {}).netboot.x86_64-linux;
-  in {
+    netboot = let build = (import <nixpkgs/nixos/lib/eval-config.nix> {
+        system = "x86_64-linux";
+        modules = [
+          <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix>
+           { boot.kernelParams = [ "console=ttyS0,115200n8" ]; }
+        ];
+      }).config.system.build;
+    in pkgs.symlinkJoin {
+      name="netboot";
+      paths=[
+        build.netbootRamdisk
+        build.kernel
+        build.netbootIpxeScript
+      ];
+      postBuild = ''
+        mkdir -p $out/nix-support
+        echo "file bzImage $out/bzImage" >> $out/nix-support/hydra-build-products
+        echo "file initrd $out/initrd" >> $out/nix-support/hydra-build-products
+        echo "file ipxe $out/netboot.ipxe" >> $out/nix-support/hydra-build-products
+      '';
+    };
+   in {
     enable = true;
     path = pkgs.runCommand "dhcp-pxe-root" {}
       ''
