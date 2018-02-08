88c602c06d3bc136a301927d34bfa93498fbe60b
Graham Christensen <graham@grahamc.com>
2018-02-07 19:31:43 -0500

    nix.ci

--- zoidberg/default.nix
+++ zoidberg/default.nix
@@ -255,6 +255,12 @@ in { pkgs, ... }: {
           locations = (vhostPHPLocations pkgs "${src.ofborg.php}/web");
         });
 
+        "nix.ci" = defaultVhostCfg // {
+          enableACME = true;
+          forceSSL = true;
+          root = ./nix.ci;
+        };
+
         "gsc.io" = defaultVhostCfg // {
           #enableACME = true;
           #forceSSL = true;
