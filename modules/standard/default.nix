1c00b426f1581726f1e94ee314bdfad0ce3d0063
Graham Christensen <graham@grahamc.com>
2018-02-07 19:30:32 -0500

    Update prometheus node exporter to collect CPU and nixos version info

--- modules/standard/default.nix
+++ modules/standard/default.nix
@@ -5,7 +5,23 @@ let
   secrets = import ../../secrets.nix;
 in {
   config = {
-    nixpkgs.config.allowUnfree = true;
+    nixpkgs = {
+      config = {
+        allowUnfree = true;
+        packageOverrides = pkgs: {
+          prometheus-node-exporter = pkgs.prometheus-node-exporter.overrideAttrs (x: {
+            # Update from 17.09's 0.14.0 because it lacked CPU metric support
+            src = pkgs.fetchFromGitHub {
+              rev = "v0.15.0";
+              owner = "prometheus";
+              repo = "node_exporter";
+              sha256 = "0v1m6m9fmlw66s9v50y2rfr5kbpb9mxbwpcab4cmgcjs1y7wcn49";
+            };
+          });
+        };
+      };
+    };
+
     services.openssh = {
       enable = true;
       passwordAuthentication = false;
@@ -72,9 +88,37 @@ in {
     services.prometheus.nodeExporter = {
       enable = true;
       enabledCollectors = [
+        # "cpu" # broken?
         "bonding" "systemd" "diskstats" "filesystem" "netstat" "meminfo"
+        "textfile"
+      ];
+      extraFlags = [
+        "--collector.textfile.directory=/var/lib/prometheus-node-exporter-text-files"
+        ""
       ];
     };
 
+    system.activationScripts.node-exporter-system-version = ''
+      mkdir -pm 0775 /var/lib/prometheus-node-exporter-text-files
+      (
+        cd /var/lib/prometheus-node-exporter-text-files
+        (
+          echo -n "system_version ";
+          readlink /nix/var/nix/profiles/system | cut -d- -f2
+        ) > system-version.prom.next
+        mv system-version.prom.next system-version.prom
+      )
+
+    '';
+
+    # Ugh, delete this garbage!
+    systemd.services.prometheus-node-exporter.script = lib.mkForce (let
+        cfg = config.services.prometheus.nodeExporter;
+      in ''
+        exec ${pkgs.prometheus-node-exporter}/bin/node_exporter \
+          ${lib.concatMapStringsSep " " (x: "--collector." + x) cfg.enabledCollectors} \
+          --web.listen-address ${cfg.listenAddress}:${toString cfg.port} \
+          ${lib.concatStringsSep " \\\n  " cfg.extraFlags}
+      '');
   };
 }
