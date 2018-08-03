3d182167185edfe88b2a929969e47e3dca498661
Graham Christensen <graham@grahamc.com>
2018-08-03 07:31:02 -0400

    addotr to weecht

--- zoidberg/default.nix
+++ zoidberg/default.nix
@@ -107,7 +107,43 @@ in { pkgs, ... }: {
   environment = {
     systemPackages = with pkgs; [
       git # for kylechristensen
-      weechat
+      (weechat.override {
+        configure = { availablePlugins, ... }: {
+          plugins = [
+            (availablePlugins.python.withPackages (ps: [
+              (ps.potr.overridePythonAttrs (oldAttrs:
+                {
+                  propagatedBuildInputs = [
+                    (ps.buildPythonPackage rec {
+                      name = "pycrypto-${version}";
+                      version = "2.6.1";
+
+                      src = pkgs.fetchurl {
+                        url = "mirror://pypi/p/pycrypto/${name}.tar.gz";
+                        sha256 = "0g0ayql5b9mkjam8hym6zyg6bv77lbh66rv1fyvgqb17kfc1xkpj";
+                      };
+
+                      patches = pkgs.stdenv.lib.singleton (pkgs.fetchpatch {
+                        name = "CVE-2013-7459.patch";
+                        url = "https://anonscm.debian.org/cgit/collab-maint/python-crypto.git"
+                          + "/plain/debian/patches/CVE-2013-7459.patch?h=debian/2.6.1-7";
+                        sha256 = "01r7aghnchc1bpxgdv58qyi2085gh34bxini973xhy3ks7fq3ir9";
+                      });
+
+                      buildInputs = [ pkgs.gmp ];
+
+                      preConfigure = ''
+                        sed -i 's,/usr/include,/no-such-dir,' configure
+                        sed -i "s!,'/usr/include/'!!" setup.py
+                      '';
+                    })
+                  ];
+                }
+              ))
+            ]))
+          ];
+        };
+      })
       screen
       tmux
       aspell
