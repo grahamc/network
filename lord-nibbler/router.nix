d37b339a72c3fe82401318aecaba8c42b85ab24d
Graham Christensen <graham@grahamc.com>
2019-05-01 21:45:40 -0400

    Pin; everything can get IPs, we can ping out but hosts behind the router don't get replies

--- lord-nibbler/router.nix
+++ lord-nibbler/router.nix
@@ -380,8 +380,6 @@ in lib.concatStrings [
          AdvSendAdvert on;
          prefix ::/64
          {
-              AdvOnLink on;
-              AdvAutonomous on;
          };
       };
 
@@ -390,112 +388,21 @@ in lib.concatStrings [
          AdvSendAdvert on;
          prefix ::/64
          {
-              AdvOnLink on;
-              AdvAutonomous on;
          };
       };
     '';
   };
 
   networking.dhcpcd.extraConfig = ''
-  xidhwaddr
-  noipv6rs
-  #debug
-  interface enp1s0
-    #ipv6rs
-    iaid 10
-    #ia_na 1
-    ia_pd 2/::/56 enp2s0/2 nougatwifi/3
-  '';
-
-  /*if true then ''
-noipv6rs;
-debug;
-
-interface enp1s0 {
-        ipv6rs;
-        send ia-na 1;
-        send ia-pd 2;
-};
-
-
-id-assoc pd 2 {
-        prefix-interface enp2s0 {
-                sla-id 0;
-                ifid 1;
-                sla-len 16;
-        };
-        prefix-interface nougatwifi {
-#               sla-id 0;
-                sla-id 15;
-                sla-len 16;
-        };
-};
-
-id-assoc na 1 {
-
-};
-
-  ''
- else ''
-    interface ${externalInterface} {
+    xidhwaddr
+    noipv6rs
+    debug
+    interface enp1s0
+      #ipv6rs
+      iaid 10
       ia_na 1
-      ia_pd 2
-    };
-
-    id-assoc na 1 {};
-
-    id-assoc pd 2 {
-      prefix ::/52 infinity;
-
-      prefix-interface lo {
-        sla-id 0;
-        sla-len 0;
-      };
-
-      prefix-interface ${vlans.nougat.name} {
-        sla-id 1;
-        sla-len 8;
-      };
-
-      prefix-interface ${vlans.nougat-wifi.name} {
-        sla-id 2;
-        sla-len 8;
-      };
-    };
-
-  '';*/
-  /*
-  id-assoc na 2 {};
-
-    id-assoc pd 3 {
-      prefix ::/56 infinity;
-
-      prefix-interface lo {
-        sla-id 0;
-        sla-len 0;
-      };
-
-      prefix-interface ${vlans.nougat.name} {
-        sla-id 1;
-        sla-len 8;
-      };
-
-      prefix-interface ${vlans.nougat-wifi.name} {
-        sla-id 2;
-        sla-len 8;
-      };
-    };
-
-    #noipv6rs
-    #interface ${externalInterface}
-    #ia_na 1
-    #ia_na 2
-    #ia_na 3
-    #ia_pd 1/::/56 ${vlans.nougat.name}/1/64  ${vlans.nougat-wifi.name}/2/64
-
+      ia_pd 2/::/56 enp2s0/2 nougatwifi/3
   '';
-*/
 
   services.dhcpd4 = {
     enable = true;
--- ogden/wireguard.nix
+++ ogden/wireguard.nix
@@ -3,20 +3,23 @@ let
   privatekey = config.networking.wireguard.interfaces.wg0.privateKeyFile;
   publickey = "${dirOf privatekey}/public";
 in {
-  networking.firewall.allowedUDPPorts = [ 51820 ];
+  networking.firewall.allowedUDPPorts = [ 41741 ];
   networking.wireguard.interfaces.wg0 = {
     ips = [ "10.10.2.15/24" ];
     privateKeyFile = "/etc/wireguard/private";
+    listenPort = 41741;
 
     peers = [
       {
+        # petunia
         publicKey = "iRqkVDUccM1duRrG02a9IraBgR9zew6SqAclqUaLoyI=";
         allowedIPs = [ "10.10.2.10/32" ];
       }
       {
+        # zoidberg
         publicKey = "BQ7+bGuKVat/I8b1s75eKlRAE3PwD9DTTbOJ4yUEAzo=";
-        allowedIPs = [ "10.10.2.5/32" "10.10.2.10/32" ];
-        endpoint = "147.75.97.237:51820";
+        allowedIPs = [ "10.10.2.5/32" ];
+        endpoint = "gsc.io:51820";
         persistentKeepalive = 25;
       }
     ];
