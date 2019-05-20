8962a02b202ef2f5c231abaa48a331f8528d57cd
Graham Christensen <graham@grahamc.com>
2019-05-20 12:01:58 -0400

    flexo: set ACME certs to my account by default

--- flexo/default.nix
+++ flexo/default.nix
@@ -1,5 +1,5 @@
 { secrets }:
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./hardware.nix
@@ -7,15 +7,23 @@
     ./grahams-websites.nix
   ];
 
-  services.nginx = {
-    logError = "syslog:server=unix:/dev/log";
+  options.security.acme.certs = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule {
+      config.email = lib.mkDefault "graham@grahamc.com";
+    });
+  };
 
-    appendHttpConfig = ''
-      log_format combined_host '$host $remote_addr - $remote_user [$time_local] '
+  config = {
+    services.nginx = {
+      logError = "syslog:server=unix:/dev/log";
+
+      appendHttpConfig = ''
+        log_format combined_host '$host $remote_addr - $remote_user [$time_local] '
                      '"$request" $status $bytes_sent '
                      '"$http_referer" "$http_user_agent" "$gzip_ratio"';
 
-      access_log syslog:server=unix:/dev/log combined_host;
-    '';
+        access_log syslog:server=unix:/dev/log combined_host;
+      '';
+    };
   };
 }
